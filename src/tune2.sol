5154346aa5de23061b372f2625a7847d11d70fa3
Lev Livnev <lev.livnev@gmail.com>
2018-08-03 00:14:02 +0100

    Squashed commit of the following:
    commit cd43b65bf18a9a2a0ac367a1aa9444a016bad897
    Author: Lev Livnev <lev.livnev@gmail.com>
    Date:   Fri Aug 3 00:12:23 2018 +0100
    
        fix typos.
    
    commit d90fa47df9aa26b07b8a53b480d623556e5078ec
    Author: Lev Livnev <lev.livnev@gmail.com>
    Date:   Fri Aug 3 00:00:13 2018 +0100
    
        assembly version of fold and imul.
    
    commit 800ced61cea577cfa4cf3135ddf7feaf6718ee4d
    Author: Lev Livnev <lev.livnev@gmail.com>
    Date:   Thu Aug 2 23:53:07 2018 +0100
    
        assembly version of grab.
    
    commit 71b247fef469ea7641077e6183955251c9eeb89b
    Author: Lev Livnev <lev.livnev@gmail.com>
    Date:   Thu Aug 2 23:48:25 2018 +0100
    
        assembly version of tune.
    
    commit aa15f9989bd60a8b96533bf525d67ac0cd481b53
    Author: Lev Livnev <lev.livnev@gmail.com>
    Date:   Thu Aug 2 23:25:43 2018 +0100
    
        assembly versions of root, era, dai, sin, vice, Tab, Gem, Ink, Art, file, move, slip, heal

--- src/tune2.sol
+++ src/tune2.sol
@@ -30,73 +30,233 @@ contract Vat {
       if lt(sig, 0x7cdd3fde/*   function slip(bytes32 ilk, address guy, int256 wad) external; */) {
         if lt(sig, 0x3af39c21/*   function undefined() external; */) {
           if lt(sig, 0x2424be5c/*   function urns(bytes32 , address ) external returns (int256 gem, int256 ink, int256 art); */) {
-            if eq(sig, 0x11045bee /*   function grab(bytes32 ilk, address lad, address vow, int256 dink, int256 dart) external; */) {
+            if eq(sig, 0x11045bee /*   function grab(bytes32 ilk, address lad, address vow, int256 dink, int256 dart) external; */)
+	    {		
+                let hash_0 := hash3(4, calldataload(4), calldataload(36))
 
+		// set urns[ilk][lad].ink += dink
+		sstore(add(hash_0, 1), iadd(sload(add(hash_0, 1)), calldataload(100)))
+
+		// set urns[ilk][lad].art += dart
+		sstore(add(hash_0, 2), iadd(sload(add(hash_0, 2)), calldataload(132)))	
+
+		let hash_1 := hash2(3, calldataload(4))
+
+		// set ilks[ilk].Art += dart
+		sstore(add(hash_1, 1), iadd(sload(add(hash_1, 1)), calldataload(132)))
+
+		let hash_2 := hash2(2, calldataload(68))
+
+		// set sin[vow] -= i.rate * dart
+		sstore(hash_2, iadd(sload(hash_2), imul(sload(hash_1), calldataload(132))))
+
+		// set vice -= i.rate * dart
+		sstore(6, iadd(sload(6), imul(sload(hash_1), calldataload(132))))
+
+		stop()
             }
             if eq(sig, 0x143e55e0 /*   function era() external returns (uint48); */) {
-
+	      mstore(64, timestamp)
+	      return(64, 6)
             }
           }
-          if eq(sig, 0x2424be5c /*   function urns(bytes32 , address ) external returns (int256 gem, int256 ink, int256 art); */) {
-
+          if eq(sig, 0x2424be5c /*   function urns(bytes32 , address ) external returns (int256 gem, int256 ink, int256 art); */)
+	  {
+	      // TODO urns returns struct
           }
           if eq(sig, 0x2d61a355 /*   function vice() external returns (int256); */) {
-
+	     mstore(64, sload(6))
+	     return(64, 32)
           }
         }
         if lt(sig, 0x673c17da/*   function Art(bytes32 ilk, address lad) external returns (int256); */) {
-          if eq(sig, 0x3af39c21 /*   function undefined() external; */) {
-
+          if eq(sig, 0x3af39c21 /*   function undefined() external; */)
+	  {
+	      // wat?
           }
-          if eq(sig, 0x4186706d /*   function Gem(bytes32 ilk, address lad) external returns (int256); */) {
-
+          if eq(sig, 0x4186706d /*   function Gem(bytes32 ilk, address lad) external returns (int256); */)
+	  {
+	      let hash_0 := hash3(4, calldataload(4), calldataload(36))
+	      mstore(64, sload(hash_0))
+	      return(64, 32)
           }
         }
-        if eq(sig, 0x673c17da /*   function Art(bytes32 ilk, address lad) external returns (int256); */) {
-
+        if eq(sig, 0x673c17da /*   function Art(bytes32 ilk, address lad) external returns (int256); */)
+	{
+	    // lol overflow risk??	
+	    let hash_0 := add(hash3(4, calldataload(4), calldataload(36)), 2)
+	    mstore(64, sload(hash_0))
+	    return(64, 32)
         }
         if eq(sig, 0x6c25b346 /*   function dai(address ) external returns (int256); */) {
-
+	    let hash_0 := hash2(1, calldataload(4))
+	    mstore(64, sload(hash_0))
+	    return(64, 32)
         }
-        if eq(sig, 0x71854745 /*   function Ink(bytes32 ilk, address lad) external returns (int256); */) {
-
+        if eq(sig, 0x71854745 /*   function Ink(bytes32 ilk, address lad) external returns (int256); */)
+	{
+	    // lol overflow risk??
+	    let hash_0 := add(hash3(4, calldataload(4), calldataload(36)), 1)
+	    mstore(64, sload(hash_0))
+	    return(64, 32)
         }
       }
       if lt(sig, 0xd9638d36/*   function ilks(bytes32 ) external returns (int256 rate, int256 Art); */) {
         if lt(sig, 0xa4593c52/*   function tune(bytes32 ilk, address lad, int256 dink, int256 dart) external; */) {
-          if eq(sig, 0x7cdd3fde /*   function slip(bytes32 ilk, address guy, int256 wad) external; */) {
+          if eq(sig, 0x7cdd3fde /*   function slip(bytes32 ilk, address guy, int256 wad) external; */)
+	  {
+	    let hash_0 := hash3(4, calldataload(4), calldataload(36))
+	    
+	    // set urns[ilk][guy].gem = urns[ilk][guy].gem + wad
+	    sstore(hash_0, iadd(sload(hash_0), calldataload(68)))
 
+	    stop()
+	  }
+          if eq(sig, 0x815d245d /*   function file(bytes32 ilk, bytes32 what, int256 risk) external; */)
+	  {
+	    let hash_0 := hash2(3, calldataload(4))
+	    // TODO: string literals in assembly?
+	    // if what == "rate" set ilks[ilk].rate = risk
+	    if eq(calldataload(36), "rate") { sstore(hash_0, calldataload(68)) }
+
+	    stop()
           }
-          if eq(sig, 0x815d245d /*   function file(bytes32 ilk, bytes32 what, int256 risk) external; */) {
-
-          }
         }
-        if eq(sig, 0xa4593c52 /*   function tune(bytes32 ilk, address lad, int256 dink, int256 dart) external; */) {
+        if eq(sig, 0xa4593c52 /*   function tune(bytes32 ilk, address lad, int256 dink, int256 dart) external; */)
+	{
+            let hash_0 := hash3(4, calldataload(4), calldataload(36))
 
+	    // set urns[ilk][lad].gem -= dink
+	    sstore(hash_0, isub(sload(hash_0), calldataload(68)))
+	    
+	    // set urns[ilk][lad].ink += dink
+	    sstore(add(hash_0, 1), iadd(sload(add(hash_0, 1)), calldataload(68)))
+
+	    // set urns[ilk][lad].art += dart
+	    sstore(add(hash_0, 2), iadd(sload(add(hash_0, 2)), calldataload(100)))	
+
+	    let hash_1 := hash2(3, calldataload(4))
+	    
+	    // set ilks[ilk].Art += dart
+	    sstore(add(hash_1, 1), iadd(sload(add(hash_1, 1)), calldataload(100)))
+
+	    let hash_2 := hash2(1, calldataload(36))
+
+	    // set dai[lad] += i.rate * dart
+	    sstore(hash_2, iadd(sload(hash_2), imul(sload(hash_1), calldataload(100))))
+	    
+	    // set Tab += i.rate * dart
+	    sstore(5, iadd(sload(5), imul(sload(hash_1), calldataload(100))))
+
+	    stop()
         }
-        if eq(sig, 0xb65337df /*   function fold(bytes32 ilk, address vow, int256 rate) external; */) {
+        if eq(sig, 0xb65337df /*   function fold(bytes32 ilk, address vow, int256 rate) external; */)
+	{
+	    let hash_0 := hash2(3, calldataload(4))
 
+	    // set i.rate += rate
+	    sstore(hash_0, iadd(sload(hash_0), calldataload(68)))
+
+	    // rad := i.Art * rate
+	    let rad := imul(sload(add(hash_0, 1)), calldataload(68))
+
+	    let hash_1 := hash2(1, calldataload(36))
+
+	    // set dai[vow] += rad
+	    sstore(hash_1, iadd(sload(hash_1), rad))
+
+	    // set Tab += rad
+	    sstore(5, iadd(sload(5), rad))
+
+	    stop()
         }
-        if eq(sig, 0xbb35783b /*   function move(address src, address dst, uint256 wad) external; */) {
+        if eq(sig, 0xbb35783b /*   function move(address src, address dst, uint256 rad) external; */) {
 
+	   let hash_0 := hash2(1, calldataload(4))
+
+	   // dai_src := dai[src]
+	   let dai_src := sload(hash_0)
+
+	   // uhh, nervous about the uint here...
+	   // TODO: need to add another check!
+	   // iff dai_src >= rad
+	   if slt(dai_src, calldataload(68)) { revert(0, 0) }
+
+	   // set dai[src] = dai_src - rad
+	   sstore(hash_0, isub(dai_src, calldataload(68)))
+
+	   let hash_1 := hash2(1, calldataload(36))
+
+	   // dai_dst := dai[dst]
+	   let dai_dst := sload(hash_1)
+
+	   // set dai[dst] = dai_dst + rad
+	   sstore(hash_1, iadd(dai_dst, calldataload(68)))
+
+	   stop()
         }
       }
       if lt(sig, 0xebf0c717/*   function root() external returns (address); */) {
-        if eq(sig, 0xd9638d36 /*   function ilks(bytes32 ) external returns (int256 rate, int256 Art); */) {
-
+        if eq(sig, 0xd9638d36 /*   function ilks(bytes32 ) external returns (int256 rate, int256 Art); */)
+	{
+	    // TODO ilks returns tuple
         }
         if eq(sig, 0xdc42e309 /*   function Tab() external returns (int256); */) {
-
+	   mstore(64, sload(5))
+	   return (64, 32)
         }
       }
       if eq(sig, 0xebf0c717 /*   function root() external returns (address); */) {
-
+      	 mstore(64, sload(0))
+	 return (64, 32)
       }
-      if eq(sig, 0xee8cd748 /*   function heal(address u, address v, int256 wad) external; */) {
+      if eq(sig, 0xee8cd748 /*   function heal(address u, address v, int256 rad) external; */) {
+      	 let hash_0 := hash2(2, calldataload(4))
 
+	 // sin_u := sin[u]
+	 let sin_u := sload(hash_0)
+
+	 // iff sin_u >= rad
+	 if slt(sin_u, calldataload(68)) { revert(0, 0) }
+	 
+	 let hash_1 := hash2(1, calldataload(36))
+
+	 // dai_v := dai[v]
+	 let dai_v := sload(hash_1)
+
+	 // iff dai_v >= rad
+	 if slt(dai_v, calldataload(68)) { revert(0, 0) }
+
+	 // _vice := vice
+	 let _vice := sload(6)
+
+	 // iff _vice >= rad
+	 if slt(_vice, calldataload(68)) { revert(0, 0) }
+	 
+	 // _Tab := Tab
+	 let _Tab := sload(5)
+
+	 // iff _Tab >= rad
+	 if slt(_Tab, calldataload(68)) { revert(0, 0) }
+
+	 // set sin[u] = sin_u - rad
+	 sstore(hash_0, isub(sin_u, calldataload(68)))
+
+	 // set dai[v] = sin_u - rad
+	 sstore(hash_1, isub(dai_v, calldataload(68)))
+
+	 // set vice = _vice - rad
+	 sstore(6, isub(_vice, calldataload(68)))
+
+	 // set Tab = _Tab - rad 
+	 sstore(5, isub(_Tab, calldataload(68)))
+
+	 stop()
       }
       if eq(sig, 0xf059212a /*   function sin(address ) external returns (int256); */) {
-
+	 let hash_0 := hash2(2, calldataload(4))
+	 mstore(64, sload(hash_0))
+	 return(64, 32)
       }
       function hash2(b, i) -> h {
         mstore(0, b)
@@ -111,11 +271,21 @@ contract Vat {
         h := keccak256(0, 64)
       }
       function iadd(x, y) -> z {
-        z := add(x, y);
-        if and(or(jkgg),
-               or())
-        // TODO
+        z := add(x, y)
+        if iszero(or(iszero(sgt(y, 0)), sgt(z, x))) { revert(0, 0) }
+        if iszero(or(iszero(slt(y, 0)), slt(z, x))) { revert(0, 0) }
       }
+      function isub(x, y) -> z {
+        let minus_pow255 := sub(0, 57896044618658097711785492504343953926634992332820282019728792003956564819968)
+      	if eq(y, minus_pow255) { revert(0, 0) }
+	z := iadd(x, sub(0, y))
+      }
+      function imul(x, y) -> z {
+        z := mul(x, y)
+	let minus_pow255 := sub(0, 57896044618658097711785492504343953926634992332820282019728792003956564819968)
+	if iszero(or(iszero(slt(y, 0)), iszero(eq(x, minus_pow255)))) { revert(0, 0) }
+	if iszero(or(eq(y, 0), eq(div(z, y), x))) { revert(0, 0) }
     }
   }
+}
 }
